46237db53d63f6f2afd1177afc9f5dcc1ff9ca64
Yash Sharma <b20241@students.iitmandi.ac.in>
2021-10-23 17:39:09 +0530

    engine changed to ejs

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ This is an management application for students to register and login and raise c
 5. Aayushmaan Jha
 6. Vastav Bansal
 7. Vishwas Garg
+
+For those who are not aware of how to solve this error using Windows PowerShell
+
+Open PowerShell (Run As Administrator)
+Check the current execution policy using this command
+    Get-ExecutionPolicy
+    # You should get 'Restricted'
+Run this command to make it 'Unrestricted'
+    Set-ExecutionPolicy Unrestricted
+Check again whether execution policy changed by running this command
+    Get-ExecutionPolicy
+    # You should get 'Unrestricted'
+Now try to run nodemon on your project
+    nodemon 'filename.js'
+Hope this would be helpful
--- app.js
+++ app.js
@@ -3,25 +3,33 @@ const express = require('express');
 // express app
 const app = express();
 
+// Static Files
+app.use(express.static('public'));
+
+// register view engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
-
 app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', { root: __dirname });
+  res.render('login', { title: 'Login'});
+});
+app.get('/login.ejs', (req, res) => {
+  res.render('login', { title: 'Login'});
 });
 
-app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+
+app.get('/register.ejs', (req, res) => {
+  res.render('register', { title: 'Sign up' });
 });
 
 // redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/complaint_register.ejs', (req, res) => {
+  res.render('complaint_register', { title: 'Complaint' });
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404', { title: '404' });
 });
